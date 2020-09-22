26cf9dffae23d1f90d5e56d9d96e33170e54ef1d
Safna <safna.usa@gmail.com>
2020-09-22 18:15:28 -0500

    ExerciseOne Completed

--- src/main/scala/Utilities.scala
+++ src/main/scala/Utilities.scala
@@ -2,6 +2,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
 object Utilities {
+
   def setupLogging() = {
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
   }
@@ -12,8 +13,25 @@ object Utilities {
     StructField("overall", DoubleType, nullable = true),
     StructField("summary", StringType, nullable = true),
   ))
-
   /*
    * Write your Yelp Schema below
    */
+
+  val yelpReviewsSchema = StructType(Array(
+    StructField("name", StringType, nullable = true),
+    StructField("city", StringType, nullable = true),
+    StructField("stars", DoubleType, nullable = true),
+    StructField("review_count", IntegerType, nullable = true),
+    StructField("hours", StructType(
+      Array(
+        StructField("Monday", StringType),
+        StructField("Tuesday", StringType),
+        StructField("Wednesday", StringType),
+        StructField("Thursday", StringType),
+        StructField("Friday", StringType),
+        StructField("Saturday", StringType),
+        StructField("Sunday", StringType)
+      )
+    )),
+  ))
 }
